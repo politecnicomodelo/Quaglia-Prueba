a8acf3f381029c6b50d7d52a914dcd80dba1a55d
lvvittor-local <lvvittor@gmail.com>
2017-03-31 11:48:08 -0300

    Ejercicio Fixture con clases creadas y con todo agregado

--- Clases/Curling.py
+++ Clases/Curling.py
@@ -1,7 +1,8 @@
 class Jugador(object):
-    Nombre=""
-    Fechanac= none
-    Numero=0
+
+    Nombre = ""
+    Fechanac = None
+    Numero = 0
 
     def setNombre(self, nombre):
         self.Nombre=nombre
@@ -12,10 +13,70 @@ class Jugador(object):
     def setNumero(self, numero):
         self.Numero=numero
 
-class Equipo(objet):
+class Equipo(object):
 
-    Nombre=""
-    Barrio=""
-    Disponibilidad=[]
-    listaJugadores=[]
+    Nombre = ""
+    Barrio = ""
+    Capitan = ""
+    Disponibilidad = []
+    listaJugadores = []
+
+    def setNombre(self, nombre):
+        self.Nombre=nombre
+
+    def setBarrio(self, barrio):
+        self.Barrio=barrio
+
+    def setCapitan(self,numero):
+        self.Capitan=numero
+
+    def __init__(self):
+        self.Disponibilidad=[]
+        self.listaJugadores=[]
+
+    def agregarJugador(self, jugador):
+        for JugAct in self.listaJugadores:
+            if JugAct.Numero == Jugador.Numero:
+                return False
+
+        (self.listaJugadores).append(jugador)
+
+    def setDisponibilidad(self,turno):
+        (self.Disponibilidad).append(turno)
+
+class Partido(object):
+    Dia = None
+    Turno = ""
+    Equipos = []
+
+    def __init__(self):
+        self.Equipos=[]
+
+    def setDia(self, dia):
+        self.Dia=dia
+
+    def setTurno(self,turno):
+        self.Turno=turno
+
+    def setEquipo(self, equipo):
+        (self.Equipos).append=[]
+
+class Torneo(object):
+
+    Equipos = []
+    listaPartidos = []
+
+    def __init__(self):
+        self.listaEquipos = []
+        self.listaPartidos = []
+
+    def agregarEquipo(self, equipo):
+        for EquipoAct in self.listaEquipos:
+            if EquipoAct.Nombre == Equipo.Nombre:
+                return False
+        (self.Equipos).append(equipo)
+
+    def agregarPartido(self,partido):
+        (self.listaPartidos).append(partido)
+
 
--- kodi.py
+++ kodi.py
@@ -1,1 +1,78 @@
+from Clases.Curling import *
+from datetime import date
+
+Jug1=Jugador()
+Jug2=Jugador()
+Jug3=Jugador()
+Jug4=Jugador()
+Jug5=Jugador()
+
+Jug1.setNombre("eemesi")
+Jug1.setNumero(10)
+
+
+Jug6=Jugador()
+Jug7=Jugador()
+Jug8=Jugador()
+Jug9=Jugador()
+Jug10=Jugador()
+
+Jug6.setNombre("jesus")
+Jug6.setNumero(33)
+
+Equipo1 = Equipo()
+
+Equipo1.setNombre("Chaca")
+Equipo1.setBarrio("San Martin")
+Equipo1.setCapitan(10)
+
+TNP=[False,False,False] #Turno no puede en ningun horiario
+TM=[True,False,False]   #Turno mañana
+TT=[False,True,False]   #Turno tarde
+TN=[False,False,True]   #Turno noche
+TMT=[True,True,False]   #Turno mañana y tarde
+TMN=[True,False,True]   #Turno mañana y noche
+TTN=[False,True,True]   #Turno tarde y noche
+TMTN=[True,True,True]   #Turno mañana, tarde y noche
+
+Equipo1.setDisponibilidad(TM)   #Lunes
+Equipo1.setDisponibilidad(TT)   #Martes
+Equipo1.setDisponibilidad(TN)   #Miercoles
+Equipo1.setDisponibilidad(TMN)  #Jueves
+Equipo1.setDisponibilidad(TTN)  #Viernes
+Equipo1.setDisponibilidad(TMTN) #Sabado
+Equipo1.setDisponibilidad(TNP)  #Domingo
+
+
+Equipo1.agregarJugador(Jug1)    #Agrego jugadores
+Equipo1.agregarJugador(Jug2)
+Equipo1.agregarJugador(Jug3)
+Equipo1.agregarJugador(Jug4)
+Equipo1.agregarJugador(Jug5)
+
+Equipo2=Equipo()
+
+Equipo2.agregarJugador(Jug6)    #Agrego jugadores
+Equipo2.agregarJugador(Jug7)
+Equipo2.agregarJugador(Jug8)
+Equipo2.agregarJugador(Jug9)
+Equipo2.agregarJugador(Jug10)
+
+Equipo2.setNombre("Atlanta")
+Equipo2.setBarrio("Caballito")
+Equipo2.setCapitan(33)
+
+Equipo2.setDisponibilidad(TT)   #Lunes
+Equipo2.setDisponibilidad(TN)   #Martes
+Equipo2.setDisponibilidad(TNP)  #Miercoles
+Equipo2.setDisponibilidad(TMN)  #Jueves
+Equipo2.setDisponibilidad(TMTN) #Viernes
+Equipo2.setDisponibilidad(TM)   #Sabado
+Equipo2.setDisponibilidad(TNP)  #Domingo
+
+LpmCup= Torneo()
+
+LpmCup.agregarEquipo(Equipo1)   #Agrego equipos al torneo
+LpmCup.agregarEquipo(Equipo2)
+
 
